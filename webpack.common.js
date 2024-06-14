cd7f41627015d3fda0edadd55bfbc1c39f768a8d
akylik <a_kylik@ukr.net>
2024-06-14 23:07:25 +0300

    update

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,26 @@
+// const path = require('path');
+
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         path: path.resolve(__dirname, 'build'),
+//         filename: 'js/app.js'
+//     }
+// }
+
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'js/app.js'
-    }
-}
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/app.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
